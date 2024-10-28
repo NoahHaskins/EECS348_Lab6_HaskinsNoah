4cd32a5b26c18fe4e4d30617a1179e8709924fd3
Noah <nhaskin@ku.edu>
2024-10-27 22:06:06 -0500

    Add files via upload

---
+++ EECS348_Lab6_HaskinsNoah/p1script.js
@@ -0,0 +1,5 @@
+// JavaScript to interact with elements
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to my profile page!");
+    // Additional JavaScript functionality can be added here.
+});
